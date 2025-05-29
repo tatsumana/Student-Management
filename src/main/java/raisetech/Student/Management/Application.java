50296c59b22b10f66ddf38f2212960603065789d
tatsumana <tatuma1017@gmail.com>
2025-05-29 11:19:54 +0900

    管理のmap

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -2,20 +2,31 @@ package raisetech.Student.Management;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+	private Map<String, String> studentMap = new ConcurrentHashMap<>();
+
 	public static void main(String[] args) {
-		// localhost:8080
 		SpringApplication.run(Application.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String	hello(){
-		return "Hello,World";
+	// 学生情報の取得
+	@GetMapping("/studentInfo")
+	public Map<String, String> getStudentInfo() {
+		return studentMap;
+	}
+
+	// 学生情報の追加・更新
+	@PostMapping("/studentInfo")
+	public String setStudentInfo(@RequestParam String name, @RequestParam String age) {
+		studentMap.put(name, age);
+		return "登録・更新しました: " + name + " " + age + "歳";
 	}
 }
